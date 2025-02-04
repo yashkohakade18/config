986110ce18b88423b61ce8fe62b3fbccbe3dc284
yashkohakade18 <yashkohakade@gmail.com>
2025-02-04 12:52:46 +0530

    1st

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ function App() {
   return (
     <>
     <div className='bg-amber-100 min-h-screen'>
-    <h1 className=' text-center text-5xl  mx-10 text-orange-400 font-bold'>{WEBSITE_NAME}</h1>
-      <p className='  text-lg text-orange-500 mx-10 my-5 '>{WEBSITE_DESCRIPTION}</p>
+    <h1 className=' text-center text-5xl  mx-10 text-grey-400 font-bold'>{WEBSITE_NAME}</h1>
+      <p className='  text-lg text-grey-500 mx-10 my-5 '>{WEBSITE_DESCRIPTION}</p>
     
     <img src={HEADER_IMG} className='h-{200px} object-cover  mx-auto rounded-md shadow-black shadow-lg mb-6'></img>
     <div className='flex justify-center  '>
-      <button className=' m-5 px-4 py-2 text-white rounded-md'style={BUTTON_STYLES}>
+      <button className=' m-5 px-4 py-2 rounded-md'style={BUTTON_STYLES}>
 {BUTTON1}
       </button>
-      <button className=' m-5 px-4 py-2 text-white rounded-md' style={BUTTON_STYLES} >
+      <button className=' m-5 px-4 py-2 rounded-md' style={BUTTON_STYLES} >
 {BUTTON2}
       </button>
     </div>
--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
-const WEBSITE_NAME = "Ram sports🏹";
+const WEBSITE_NAME = "lakshaman sports🏹";
 const WEBSITE_DESCRIPTION = `This sports website is your ultimate destination for all things sports. We provide up-to-the-minute news, live scores, in-depth analysis, and engaging content covering a wide range of sports, from football and basketball to soccer, tennis, and more. Whether you're a die-hard fan or just looking to stay informed, our platform offers comprehensive coverage, including game highlights, player interviews, stats, and standings. Join our community of passionate sports enthusiasts and experience the thrill of the game with us.`;
 
-const HEADER_IMG = "https://img.freepik.com/premium-photo/colorful-athletic-apparel-footwear-displayed-modern-sports-storexa_767466-23343.jpg";
+const HEADER_IMG = "https://img.freepik.com/premium-photo/photo-wellorganized-sports-equipment-store_1056572-39066.jpg?";
 
 const BUTTON1 = "View More";
 const BUTTON2 = "Call Now";
 
 const BUTTON_STYLES ={
-    "background-color":"tomato",
+    "background-color":"black",
 "border-radius":"15px",
-"border":"none",
-"color": "#ffffff",
+"border":"3px solid orange",
+"color":" white" ,
 }
 
 
